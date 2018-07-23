6e4a5aebb55aebf7c7b26fe88efd5c6affea801d
clark.buskey@gmail.com <clark.buskey@gmail.com>
2018-07-23 22:56:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
     return {...driver, [key]:value};
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, valuje) {
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
   return driver;
 }
